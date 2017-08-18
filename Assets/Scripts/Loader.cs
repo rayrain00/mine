8921b29bb2e42bc875a300febb7f637f0dd44f01
桧山正晃 <09s2035m@gmail.com>
2017-08-18 14:05:23 +0900

    Loader and MovingObject class

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    #region Propertys
+    public GameObject gameManager;
+    #endregion
+
+    #region Methods
+
+    // Awake is called before loading instance
+	void Awake()
+    {
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+	}
+
+    #endregion
+}
